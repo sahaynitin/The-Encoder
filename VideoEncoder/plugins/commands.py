4337077a8887f55e6d49fdc40348b3b410234a17
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-11 13:08:58 +0530

    Update commands.py

--- VideoEncoder/plugins/commands.py
+++ VideoEncoder/plugins/commands.py
@@ -5,4 +5,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
-    
+    await add_user_to_database(c, m)
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )

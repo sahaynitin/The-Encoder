ff25398ddc078d80a2cec5e0d0ac491ffec6ee03
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-11 14:05:16 +0530

    Update add.py

--- database/add.py
+++ database/add.py
@@ -7,6 +7,6 @@ from pyrogram.types import Message
 
 
 async def add_user_to_database(bot: Client, update: Message):
-    if not await clinton.is_user_exist(update.from_user.id):
-           await clinton.add_user(update.from_user.id)
+    if not await db.is_user_exist(update.from_user.id):
+           await db.add_user(update.from_user.id)
 

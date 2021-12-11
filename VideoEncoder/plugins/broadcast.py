1a3aed9133908414fbe22ce8334d42b1ef7a7945
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-11 13:15:32 +0530

    Update broadcast.py

--- VideoEncoder/plugins/broadcast.py
+++ VideoEncoder/plugins/broadcast.py
@@ -8,7 +8,7 @@ import datetime
 import aiofiles
 import traceback
 import aiofiles.os
-from VideoEncoder.__init__ import Config
+from config import Config
 from database.database import db
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
